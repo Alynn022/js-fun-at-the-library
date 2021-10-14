f541701eb3f38bf5a77d4c8d388e61e1ad4963bf
Lynn Nguyen <AmandaN22@outlook.com>
2021-10-14 08:57:24 -0600

    Add createTitle, buildMainCharacter, saveReview

--- src/book.js
+++ src/book.js
@@ -1,11 +1,23 @@
-function createTitle() {
-  
-}
+function createTitle(bookIdea) {
+  return "The " + bookIdea;
+};
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  };
+};
+
+function saveReview(review, reviews) {
+  reviews.push(review);
+};
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
