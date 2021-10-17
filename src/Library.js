54f295c1ffd25ae4d87c906d07c57d3414f7b39a
Lynn Nguyen <AmandaN22@outlook.com>
2021-10-17 17:33:44 -0600

    Pass all Library.js

--- src/Library.js
+++ src/Library.js
@@ -20,22 +20,20 @@ function addBook(library, book) {
     return
 };
 
+//
+//  if genre is fiction
+//      access fiction shelf (array)
+//        loop through array
+//          if books title matches given title
+//            remove book from array
+//            and return appropriate string
+//  if genre is nonFiction
+//    access nonfiction shelf (array)
+//      loop through array
+//        if book title matches given title
+//        remove book from array
+//          and return appropriate string
 function checkoutBook(library, title, genre) {
-  //
-  //  if genre is fiction
-  //      access fiction shelf (array)
-  //        loop through array
-  //          if books title matches given title
-  //            remove book from array
-  //            and return appropriate string
-  //  if genre is nonFiction
-  //    access nonfiction shelf (array)
-  //      loop through array
-  //        if book title matches given title
-  //        remove book from array
-  //          and return appropriate string
-
-
   if(genre === "fiction") {
     var fictionShelf = library.shelves.fiction
     for(var i = 0; i < fictionShelf.length; i++) {
@@ -46,21 +44,24 @@ function checkoutBook(library, title, genre) {
     }
   } else if(genre === "nonFiction") {
     var nonFictionShelf = library.shelves.nonFiction
+      if(nonFictionShelf.length === 0) {
+        return (`Sorry, there are currently no copies of ${title} available at the ${library.name}`)
+    }
     for(var i = 0; i < nonFictionShelf.length; i++) {
       if(title === nonFictionShelf[i].title) {
         library.shelves.nonFiction.splice(i, 1);
         return (`You have now checked out ${title} from the ${library.name}`);
-      }
     }
-  } else {
-    return (`Sorry, there are currently no copies of ${title} at the ${libary.name}`)
-  }
-};
+      }
+    } else {
+          return (`Sorry, there are currently no copies of ${title} available at the ${library.name}`)
+        }
+      };
 
 
 
 module.exports = {
   createLibrary,
   addBook,
-  checkoutBook
+  checkoutBook,
 };
