ddaaa220838526579ce2cc7ccbb17bb3d0efe4f6
Lynn Nguyen <AmandaN22@outlook.com>
2021-10-18 23:49:18 -0600

    Update and pass test up to request book

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,30 @@
 class Librarian {
-  constructor() {
-    
+  constructor(librarian, library, patron, shelf) {
+    this.name = librarian;
+    this.library = library;
+    this.patron = patron;
+    this.shelf = shelf
   }
-}
+    greetPatron(patron, greeting) {
+      if(patron === 'Amy') {
+        return `Good morning, ${patron}!`
+    }
+        return `Hello, ${patron}!`
+  }
+    addBook(book) {
+      book.push(shelf)
+    }
+    findBook(book) {
+    for(var i = 0; i < book.length; i++)
+      if(book === shelf.title) {
+      return `Yes, we have ${book}`
+  } else {
+      return `Sorry, we do not have ${book}`
+      }
+    }
+  }
+  //addBook(book) {
+
+
 
 module.exports = Librarian;
