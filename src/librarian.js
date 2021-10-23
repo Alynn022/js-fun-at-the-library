06208c240ae16e687148c2bccb19579cbd62a95d
Lynn Nguyen <AmandaN22@outlook.com>
2021-10-23 08:05:55 -0600

    Pass calculateLateFee test

--- src/librarian.js
+++ src/librarian.js
@@ -4,25 +4,26 @@ class Librarian {
     this.library = library;
     this.patron = patron;
   }
-    greetPatron(patron, greeting) {
-      if(patron === 'Amy') {
-        return `Good morning, ${patron}!`
-      } else {
-        return `Hello, ${patron}!`
-      }
-  }
-    findBook(title) {
-      for(var i = 0; i < this.library.shelves.fantasy.length; i++) {
-       if(title === this.library.shelves.fantasy[i].title) { //this.library.shelves.fantasy is accessing the array, we need to access the object, so that goes after the [i]
-         this.library.shelves.fantasy.splice(title)
-        return `Yes, we have ${title}`
+  greetPatron(patron, greeting) {
+    if(patron === 'Amy') {
+      return `Good morning, ${patron}!`
     } else {
-        return `Sorry, we do not have ${title}`
+      return `Hello, ${patron}!`
+    }
+  }
+  findBook(title) {
+    for(var i = 0; i < this.library.shelves.fantasy.length; i++) {
+     if(title === this.library.shelves.fantasy[i].title) { //this.library.shelves.fantasy is accessing the array, we need to access the object, so that goes after the [i]
+       this.library.shelves.fantasy.splice(title)
+      return `Yes, we have ${title}`
+    }  else {
+      return `Sorry, we do not have ${title}`
       }
     }
   }
     calculateLateFee(daysLate) {
-    return Math.ceil(daysLate *= .25)
+      return Math.ceil(daysLate * .25)
+
     }
 }
 
