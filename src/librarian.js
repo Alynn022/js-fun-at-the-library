f36f29348a2afd769fbb5a79caa895b662b34fa7
Lynn Nguyen <AmandaN22@outlook.com>
2021-10-22 18:59:01 -0600

    Pass librarian tests

--- src/librarian.js
+++ src/librarian.js
@@ -1,29 +1,31 @@
 class Librarian {
-  constructor(librarian, library, patron, shelf) {
+  constructor(librarian, library, patron) {
     this.name = librarian;
     this.library = library;
     this.patron = patron;
-    this.shelf = shelf
   }
     greetPatron(patron, greeting) {
       if(patron === 'Amy') {
         return `Good morning, ${patron}!`
-    }
+      } else {
         return `Hello, ${patron}!`
+      }
   }
-    addBook(book) {
-      book.push(shelf)
-    }
-    findBook(book) {
-    for(var i = 0; i < book.length; i++)
-      if(book === shelf.title) {
-      return `Yes, we have ${book}`
-  } else {
-      return `Sorry, we do not have ${book}`
+    findBook(title) {
+      for(var i = 0; i < this.library.shelves.fantasy.length; i++) {
+       if(title === this.library.shelves.fantasy[i].title) { //this.library.shelves.fantasy is accessing the array, we need to access the object, so that goes after the [i]
+         this.library.shelves.fantasy.splice(title)
+        return `Yes, we have ${title}`
+    } else {
+        return `Sorry, we do not have ${title}`
       }
     }
   }
-  //addBook(book) {
+    calculateLateFee(daysLate) {
+    return Math.ceil(daysLate *= .25)
+    }
+}
+
 
 
 
